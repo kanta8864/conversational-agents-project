88d2c897ea8ee45965b77e38e73404830c9d57ac
Vivian <viv16mei@gmail.com>
2025-03-03 14:07:58 +0100

    added venv, class for ollama and main

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 import os
 
+from dotenv import load_dotenv
+from ollamaLLM import OllamaLLM
+
+# Load .env file
+load_dotenv()
+
+# Set model variables
+# OPENAI_BASE_URL = "https://api.openai.com/v1"
+# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+# OPENAI_ORGANIZATION = os.getenv("OPENAI_ORGANIZATION")
+
+# Initialize LangSmith
+# os.environ["LANGCHAIN_TRACING_V2"] = "true"
+# os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
+# os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+# os.environ["LANGCHAIN_PROJECT"] = "Demos"
+
+# Initialize OllamaLLM
+ollama = OllamaLLM()
+# Test OllamaLLM
+print(ollama._call("What is 2+2?"))
